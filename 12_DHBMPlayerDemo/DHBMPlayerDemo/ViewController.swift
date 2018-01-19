311bb9965674260571d0796e0aa51e2900b39560
DajuanM <252289287@qq.com>
2018-01-19 18:09:08 +0800

    12_BMPlayerDemo

---
+++ 12_DHBMPlayerDemo/DHBMPlayerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DHBMPlayerDemo
+//
+//  Created by zipingfang on 2018/1/19.
+//  Copyright © 2018年 com.denghui.demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 12_DHBMPlayerDemo/Pods/Target Support Files/Pods-DHBMPlayerDemo/Pods-DHBMPlayerDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DHBMPlayerDemo : NSObject
+@end
+@implementation PodsDummy_Pods_DHBMPlayerDemo
+@end
